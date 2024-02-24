aa28f436876b990ca02cf17cd80f3cd393236c75
Mijail Gonzalez <mrgonzalez@gbm.net>
2024-02-24 10:37:25 -0500

    media screen mobile first header

--- src/app/pages/sub-car-wash/sub-car-wash.component.ts
+++ src/app/pages/sub-car-wash/sub-car-wash.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
-
+import {AfterViewInit, Component} from '@angular/core';
+import Swiper from "swiper";
 @Component({
   selector: 'app-sub-car-wash',
   templateUrl: './sub-car-wash.component.html',
   styleUrls: ['./sub-car-wash.component.scss']
 })
-export class SubCarWashComponent {
+export class SubCarWashComponent implements AfterViewInit{
+
+  constructor() {
+
+  }
+
+  ngAfterViewInit(): void {
+    const mySwiper = new Swiper('.swiper-container', {
+      direction: 'horizontal',
+      //slidesPerView: 1,
+      loop: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        type: "bullets"
+      },
+      spaceBetween: 20
+    });
+
+
+  }
 
 }
