5263112754487729d964f0767b277d1a0e82e6f3
Mijail Gonzalez <mrgonzalez@gbm.net>
2024-02-27 18:56:57 -0500

    media screen mobile first header

--- src/app/pages/sub-barber-shop/sub-barber-shop.component.ts
+++ src/app/pages/sub-barber-shop/sub-barber-shop.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
+import Swiper from "swiper";
 
 @Component({
   selector: 'app-sub-barber-shop',
   templateUrl: './sub-barber-shop.component.html',
   styleUrls: ['./sub-barber-shop.component.scss']
 })
-export class SubBarberShopComponent {
+export class SubBarberShopComponent  implements AfterViewInit{
+  ngAfterViewInit(): void {
+    const mySwiper = new Swiper('.swiper-container', {
+      direction: 'horizontal',
+      slidesPerView: 1,
+      loop: true,
+      freeMode: true,
+      centeredSlides: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        type: "bullets"
+      },
+      spaceBetween: 20
+    });
+
+  }
 
 }
