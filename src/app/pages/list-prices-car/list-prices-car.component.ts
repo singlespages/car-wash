edbe47b1c81b924c789fb6c74a899d3c57708542
Mijail Gonzalez <mrgonzalez@gbm.net>
2024-03-12 21:58:54 -0500

    implements list prices monarca

--- src/app/pages/list-prices-car/list-prices-car.component.ts
+++ src/app/pages/list-prices-car/list-prices-car.component.ts
@@ -8,9 +8,14 @@ import { Component } from '@angular/core';
 export class ListPricesCarComponent {
 
   accordionItems = [
-    { title: 'Item 1', content: [{ nombre: 'Servicio 1', precio: 10 }, { nombre: 'Servicio 2', precio: 20 }], expanded: false },
-    { title: 'Item 2', content: [{ nombre: 'Servicio 3', precio: 15 }, { nombre: 'Servicio 4', precio: 25 }], expanded: false },
-    { title: 'Item 3', content: [{ nombre: 'Servicio 5', precio: 30 }, { nombre: 'Servicio 6', precio: 40 }], expanded: false }
+    { title: 'Taxi', content: [{ nombre: 'Lavado', precio: 15 }, { nombre: 'Servicio 2', precio: 20 }], expanded: false },
+    { title: 'Automovil', content: [{ nombre: 'Servicio 3', precio: 15 }, { nombre: 'Servicio 4', precio: 25 }], expanded: false },
+    { title: 'Camioneta', content: [{ nombre: 'Servicio 5', precio: 30 }, { nombre: 'Servicio 6', precio: 40 }], expanded: false },
+    { title: 'Camioneta gra', content: [{ nombre: 'Servicio 5', precio: 30 }, { nombre: 'Servicio 6', precio: 40 }], expanded: false },
+    { title: 'Buseta', content: [{ nombre: 'Servicio 5', precio: 30 }, { nombre: 'Servicio 6', precio: 40 }], expanded: false },
+    { title: 'Furgón peq', content: [{ nombre: 'Servicio 5', precio: 30 }, { nombre: 'Servicio 6', precio: 40 }], expanded: false },
+    { title: 'Furgón gra', content: [{ nombre: 'Servicio 5', precio: 30 }, { nombre: 'Servicio 6', precio: 40 }], expanded: false },
+    { title: 'Motos', content: [{ nombre: 'Servicio 5', precio: 30 }, { nombre: 'Servicio 6', precio: 40 }], expanded: false },
   ];
 
   toggleAccordionItem(index: number): void {
