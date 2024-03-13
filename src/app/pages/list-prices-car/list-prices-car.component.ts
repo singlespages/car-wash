fad1021e4e3390a0cdf255772ae64c37878e2db1
Mijail Gonzalez <mrgonzalez@gbm.net>
2024-03-12 21:36:32 -0500

    implements list prices monarca

--- src/app/pages/list-prices-car/list-prices-car.component.ts
+++ src/app/pages/list-prices-car/list-prices-car.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class ListPricesCarComponent {
 
+  accordionItems = [
+    { title: 'Item 1', content: [{ nombre: 'Servicio 1', precio: 10 }, { nombre: 'Servicio 2', precio: 20 }], expanded: false },
+    { title: 'Item 2', content: [{ nombre: 'Servicio 3', precio: 15 }, { nombre: 'Servicio 4', precio: 25 }], expanded: false },
+    { title: 'Item 3', content: [{ nombre: 'Servicio 5', precio: 30 }, { nombre: 'Servicio 6', precio: 40 }], expanded: false }
+  ];
+
+  toggleAccordionItem(index: number): void {
+    this.accordionItems[index].expanded = !this.accordionItems[index].expanded;
+  }
+
 }
